efea3797cb20f18bb68c0ccc0bcce44c54342a50
Jeff Seymour, Kyle Langley and Krystan Menne <kyle@>
2016-01-04 14:53:15 -0800

    replaces every e in a string with a 3

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,9 +1,23 @@
-# Behavior:
-# Input:
-# Output: 
-
 class String
-  define_method(:leetspeak) do
+  define_method(:leet_word) do |word|
+    letters = word.split(//)
+    new_letters = []
+    letters.each() do |letter|
+      if (letter == "e")
+        new_letters.push("3")
+      else
+        new_letters.push(letter)
+      end
+    end
+    new_letters.join()
+  end
 
+  define_method(:leetspeak) do
+    words = self.split(" ")
+    new_words = []
+    words.each() do |word|
+      new_words.push(leet_word(word))
+    end
+    new_words.join(" ")
   end
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -2,7 +2,10 @@ require('rspec')
 require('leetspeak')
 
 describe('String#leetspeak') do
-  it("Replaces the letter 'e' with '3'") do
-    expect(("e").leetspeak()).to(eq("3"))
+  it('returns a string as is when no Leetspeak rules apply') do
+    expect("happy".leetspeak).to(eq("happy"))
+  end
+  it('replaces every "e" in a string with a "3"') do
+    expect("elephant".leetspeak).to(eq("3l3phant"))
   end
 end
